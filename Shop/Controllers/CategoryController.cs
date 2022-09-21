7173c8c7179c0bd8b2f279f0ef9f782a59bd21b1
Dmitry Abramov <abramov.dmittry@gmail.com>
2022-09-21 11:54:57 +0300

    009 Получение списка Категорий из БД 010 Создание представления для списка Категорий 011 Добавление Категории. Часть 1 012 Добавление Категории. Часть 2

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using Shop.Data;
+using Shop.Models;
+
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Shop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+
+            return View(objList);
+        }
+
+        // GET - Create
+        public IActionResult Create()
         {
             return View();
         }
